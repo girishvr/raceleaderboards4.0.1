5e060962ec28bee68d40f93a1c6fd74c7d562912
Evans7 <evans4cod@gmail.com>
2020-07-22 13:59:18 +0530

    Added changes to resultpoolcontroller

--- grails-app/controllers/raceleaderboards1/Result_poolController.groovy
+++ grails-app/controllers/raceleaderboards1/Result_poolController.groovy
@@ -15,7 +15,7 @@ class Result_poolController {
     }
     def giveresult() {
         def db = new Sql(dataSource);
-        def query = "SELECT r.race_id as id,r.gender as gender,r.first_name as first_name,r.family_name as family_name,r.bib_number as bib_number,c.name as name,a.age_group as age_group,r.overall_position as overall_position,r.gender_position as gender_position,r.category_position as category_position,r.swim_leg_time as swim_leg_time,r.run_leg_time as run_leg_time,r.cycle_leg_time as cycle_leg_time,r.t1_leg_time as t1_leg_time,r.t2_leg_time as t2_leg_time,r.start_time as start_time,r.finish_time as finish_time,r.result_time as result_time FROM `result_pool` r,`age_group` a,`country` c WHERE r.country=c.id and r.age_category=a.id";
+        def query = "SELECT r.race_id as id,r.gender as gender,r.first_name as first_name,r.family_name as family_name,r.bib_number as bib_number,c.name as name,a.age_group as age_group,r.overall_position as overall_position,r.gender_position as gender_position,r.category_position as category_position,r.swim_leg_time as swim_leg_time,r.run_leg_time as run_leg_time,r.cycle_leg_time as cycle_leg_time,r.t1_leg_time as t1_leg_time,r.t2_leg_time as t2_leg_time,r.start_time as start_time,r.finish_time as finish_time,r.result_time as result_time FROM `result_pool` r,`age_group` a,`country` c WHERE r.country=c.id and r.age_category=a.id and r.race_id=328";
         def result1 = db.rows(query);
         renderJSONResponse(result1);
     }
