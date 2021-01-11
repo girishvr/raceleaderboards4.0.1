a28b16eaad165c215572db3bd9ca4239b436ee6f
pooja <pooja.pauskar123@gmail.com>
2021-01-11 10:28:35 +0530

    result pool

---
+++ src/test/groovy/raceleaderboards1/Result_poolNewControllerSpec.groovy
@@ -0,0 +1,18 @@
+package raceleaderboards1
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class Result_poolNewControllerSpec extends Specification implements ControllerUnitTest<Result_poolNewController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
